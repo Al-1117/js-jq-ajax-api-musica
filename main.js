0760889acbd683dfab94233e1091527449b95f30
Alioune <alioune1117@gmail.com>
2020-06-26 18:38:01 +0200

    eliminato il tasto "conferma" per fare la ricerca tramite "on.change" trovato sulla documentazione di jquery

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ $(document).ready(function() {
     {
       url: "https://flynn.boolean.careers/exercises/api/array/music",
       method: "GET",
+
+      // SE LA CHIAMATA HA SUCCESSO:
+
       success: function(richiesta){
 
         var risultatoRichiesta = richiesta.response;
@@ -26,24 +29,18 @@ $(document).ready(function() {
 
         // SELECT
 
-        $(document).on('click', '.confirm',
+        $(document).on('change', 'select',
           function(){
 
-            var valoreSelect = $('select').val();
-
-            console.log(valoreSelect);
-
-
-
-
+            ricercaGenere(risultatoRichiesta);
 
           }
         );
 
 
       },
-
-
+      // SE LA CHIAMATA FALLISCE
+      // visualizzo un errore
       error: function(){
         alert('Qualcosa è andato storto');
 
@@ -70,16 +67,71 @@ $(document).ready(function() {
 
     }
 
-  }
+  };
+
+  // FUNZIONE PER LA RICERCA DI GENERE
+  // visualizza il risultato filtrato per genere
+
+
+  function ricercaGenere(listaRicerca){
+
+    // Azzero il contenitore dei cd creati dinamicamente
+    $('.cds-container').html('');
+    // Inseririsco le variabili handlebars fuori da ciclo
+    var source = $("#music_template").html();
+    var template = Handlebars.compile(source);
+    // Inizializzo un ciclo for per fare la ricerca e
+    // trovare le corrispondenze
+    for (var i = 0; i < listaRicerca.length; i++) {
+
+      var elementiRicerca = listaRicerca[i];
+
+    // creo la variabile genere musical che sarà uguale al genre contenuto nell'elemento.
+      var genereMusicale = elementiRicerca.genre;
+
+      // Inserisco il valore dell select
+      var valoreSelect = $('select').val();
+
+      var html = template(elementiRicerca);
+
+      // Se c'è corrispondenza,
+      // appendo l'elemento al container del cds visualizzandolo
+
+      if (valoreSelect.includes(genereMusicale)){
+        $('.cds-container').append(html);
+      }
+
+
+    }
 
 
 
+    //
+    // for (var i = 0; i < risultatoRichiesta.length; i++) {
+    //   var elementiRicerca = risultatoRichiesta[i];
+    //
+    //   console.log(elementiRicerca);
+    //
+    //   for( chiave in elementiRicerca){
+    //
+    //     // console.log("l'oggetto è : " + elementiRicerca[chiave]);
+    //
+    //     var genereMusicale = elementiRicerca.genre;
+    //
+    //     var singoloCd = $(this).find('.cd');
+    //
+    //     console.log(singoloCd);
+    //
+    //
+    //     //
+    //     if (valoreSelect.includes(genereMusicale)) {
+    //       $(singoloCd).show()
+    //     } else {
+    //       $(singoloCd).hide()
+    //     }
+    //   }
 
-
-
-
-
-
+  };
 
 
 
